b7158686d0aee9272d9ca5301605a6e2910b06db
NASA High Performance Data Analytics <tpmaxwel@nccs.nasa.gov>
2020-10-23 14:06:27 -0400

    Initialize 6.x

---
+++ {{cookiecutter.github_project_name}}/{{cookiecutter.python_package_name}}/example.py
@@ -0,0 +1,14 @@
+import ipywidgets as widgets
+from traitlets import Unicode
+
+
+@widgets.register('hello.Hello')
+class HelloWorld(widgets.DOMWidget):
+    """"""
+    _view_name = Unicode('HelloView').tag(sync=True)
+    _model_name = Unicode('HelloModel').tag(sync=True)
+    _view_module = Unicode('{{ cookiecutter.npm_package_name }}').tag(sync=True)
+    _model_module = Unicode('{{ cookiecutter.npm_package_name }}').tag(sync=True)
+    _view_module_version = Unicode('^{{ cookiecutter.npm_package_version }}').tag(sync=True)
+    _model_module_version = Unicode('^{{ cookiecutter.npm_package_version }}').tag(sync=True)
+    value = Unicode('Hello World!').tag(sync=True)
